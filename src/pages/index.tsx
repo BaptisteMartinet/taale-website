6eb1f51043eac167ec1676e7b18b88ffaae154dd
Baptiste Martinet <baptiste.martinet@epitech.eu>
2023-04-23 18:08:07 +0200

    feat(pages): use Header on index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
 import type { HeadFC, PageProps } from 'gatsby';
+import { Header } from '../components/app';
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
     <main>
+      <Header />
       <h1>Home</h1>
     </main>
   );
