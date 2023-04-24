f382defa242ded5c9a2a1c9778f8f161cdd6b2b0
Baptiste Martinet <baptiste.martinet@epitech.eu>
2023-04-24 21:06:28 +0200

    feat(components/app): set maxheight on Layout components

--- src/components/app/Layout/index.tsx
+++ src/components/app/Layout/index.tsx
@@ -1,17 +1,22 @@
 import type { PropsWithChildren } from 'react';
 
 import * as React from 'react';
+import { styled } from '@mui/material';
 import { Header, Footer } from 'components/app';
 import './layout.css';
+
+const Main = styled('main')({
+  minHeight: '100vh',
+});
 
 const Layout = (props: PropsWithChildren<{}>) => {
   const { children } = props;
   return (
     <>
       <Header />
-      <main>
+      <Main>
         {children}
-      </main>
+      </Main>
       <Footer />
     </>
   );
