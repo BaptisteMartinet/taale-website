d836c82d4ca148e2dc917d05595f2028ad62d789
Baptiste Martinet <baptiste.martinet@epitech.eu>
2023-04-24 21:06:39 +0200

    feat(pages): improve 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,11 +1,23 @@
 import * as React from 'react';
+import { styled } from '@mui/material';
 import { HeadFC, PageProps } from 'gatsby';
+import { Layout } from 'components/app';
+
+const Container = styled('div')({
+  width: '100%',
+  height: '100vh',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+});
 
 const NotFoundPage: React.FC<PageProps> = () => {
   return (
-    <main>
-      <h1>Page not found</h1>
-    </main>
+    <Layout>
+      <Container>
+        <h1>Page not found</h1>
+      </Container>
+    </Layout>
   )
 };
 
